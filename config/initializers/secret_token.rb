4d593be8c34896cc635837341c734a96798f8d84
nabeel <nabeelabbas36@yahoo.com>
2013-09-11 18:47:41 +0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nabeel::Application.config.secret_key_base = '792b9d5291d84dec7f6152759ce4617bddf01b2d500c70d284bb9044a78548d260456889dba7e16aa3dc8feec7255838efee3ea5fe20f511d14e5841ee7dae41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nabeel::Application.config.session_store :cookie_store, key: '_nabeel_session'
